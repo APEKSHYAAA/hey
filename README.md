c7173d3c1dd47d32132e63ebe9cadb095b4f498a
Apekshya <apu58728@gmail.com>
2023-12-24 07:36:37 +0545

    added new detail

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # hey
+git checkout -b [name]- create new branch 
+git status- checks the status
+git add .- to add
---
+++ apu.js
@@ -0,0 +1,9 @@
+const apu = {
+    name: "Apekshya",
+    college: {
+        c_name : "kec",
+        c_add : "Dhapakhel"
+    }
+}
+apu.college.fee = 124000
+console.log(apu)
